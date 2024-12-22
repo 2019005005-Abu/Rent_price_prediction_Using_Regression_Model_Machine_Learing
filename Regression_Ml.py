fa2c10a61cc9d8ea2c3f87025833f3839b1b6cd1
Abu Al Shahriar Rifat <rifatsharda2686@gmail.com>
2024-12-23 06:42:03 +0900

    First-commit

---
+++ Regression_Ml.py
@@ -0,0 +1,55 @@
+##Regression Model
+'''
+Regression is a  technique used to to predict the numberical value on input features.
+It models  the relationship between a dependent variant  and independent variables.
+
+'''
+##Classification Model
+
+'''
+Classification is a supervised learning task that involves predicting the class of a given input data point.
+It models  the relationship between a dependent categorical variable and one or more independent variables.
+'''
+x=10;
+y=(2*x)+3;
+print(y);
+x=30;
+y=(2*x)+3;
+print(y);
+x=50;
+y=(2*x)+3;
+print(y);
+###
+'''
+y=mx+c
+F(x)=max+c;
+y=prediction
+##
+m=Sumassion(x-x bar)(y-y bar)/Sumassion(x-x)sqrt
+##Underfit Model
+##Overfit Model
+##bestfit model
+'''
+##Implementation  Model
+import pandas as pd;
+data = {'X': [10, 20, 30, 40], 'Y': [5, 15, 25, 35]}
+df = pd.DataFrame(data)
+mean_x=df['X'].mean();
+mean_y=df['Y'].mean();
+print('x-axis',mean_x);
+print('y-axis',mean_y);
+'''
+Linear Regression 
+L1 Loss & L2 Loss
+1.Mean Absolute Error(MAE)
+MAE=1/nth(|actual_values-predictions|+|actual_values-predictions|)
+2.Mean Squared Error(MSE)
+MSC=(1/nth(sqrt(actual_values-predictions))
+3.Root mean squared error(RMSC)
+RMSC=sqrt(MSC)
+'''
+
+
+
+
+
